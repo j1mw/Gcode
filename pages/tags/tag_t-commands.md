b8d96a607dcbd03bd695180d18100f84b1a637dc
Jim (Mac) <jim@fmt.com>
2020-03-29 20:35:03 +1100

    added T

---
+++ pages/tags/tag_t-commands.md
@@ -0,0 +1,11 @@
+---
+title: "T-Commands pages"
+tagName: T-Commands
+search: exclude
+permalink: tag_tcommands.html
+sidebar: gcode_sidebar
+folder: tags
+---
+{% include taglogic.html %}
+
+{% include links.html %}
--- temp/convert.py
+++ temp/convert.py
@@ -24,142 +24,160 @@ for match in header.finditer(inFile):
 	e=match.end()
 	headings.append(inFile[s:e].replace('\n',''))
 
-# Loop through headings and capture text between 
+# Special case for T-code
+tHeadings = []
+tHeadings.append("== T: Select Tool ==")
+tHeadings.append("== G-Code Background Information ==")
 
-#loop from second to n
-for i in range(1, len(headings)):
-	# Extract fileName 
-	# ----------------
+def processSection(headings):
 
-	# Is heading a combination (G0/G1 and G2/G3) ?
-	# --------------------------------------------
-	if re.search("&", headings[i -1]):
-		codeName = re.search('[M|G][0-9].*:', headings[i -1]).group(0)
-		codeName = codeName[:-1]	
-		codes = codeName.replace(' ','').split('&')
-		codeString = ''.join(map(str, codes))
-		fileName = codeString + ".md"
-		permaLink = codeString + ".html"
-	else:
-		codeName = re.search('[M|G][0-9]*', headings[i -1]).group(0)	
-		CODES.append(codeName)
+	# Loop through headings and capture text between 
 
-		x = CODES.count(codeName)
-		if x > 1:
-			fileName = codeName + "_" + str(x) + ".md"
-			permaLink = codeName + "_" + str(x) + ".html"
+	#loop from second to n
+	for i in range(1, len(headings)):
+		# Extract fileName 
+		# ----------------
+
+		# Is heading a combination (G0/G1 and G2/G3) ?
+		# --------------------------------------------
+		if re.search("T:", headings[i -1]):
+			fileName = "T.md"
+			permaLink = "T.html"
+			headerText = "Select Tool"
+			codeName = "T"
+		
+		elif re.search("&", headings[i -1]):
+			headerText = re.search('(.*?): (.*?)====',headings[i -1]).group(2)
+
+			codeName = re.search('[M|G][0-9].*:', headings[i -1]).group(0)
+			codeName = codeName[:-1]	
+			codes = codeName.replace(' ','').split('&')
+			codeString = ''.join(map(str, codes))
+			fileName = codeString + ".md"
+			permaLink = codeString + ".html"
 		else:
-			fileName = codeName + ".md"
-			permaLink = codeName + ".html"
+			headerText = re.search('(.*?): (.*?)====',headings[i -1]).group(2)
 
-	# Extract the headerText
-	# ----------------------
-	headerText = re.search('(.*?): (.*?)====',headings[i -1]).group(2)
+			codeName = re.search('[M|G][0-9]*', headings[i -1]).group(0)	
+			CODES.append(codeName)
 
-	# Extract the sectionText for each command 
-	# ----------------------------------------
-	sectionStart = inFile.index(headings[i - 1])
-	sectionEnd = inFile.index(headings[i])
+			x = CODES.count(codeName)
+			if x > 1:
+				fileName = codeName + "_" + str(x) + ".md"
+				permaLink = codeName + "_" + str(x) + ".html"
+			else:
+				fileName = codeName + ".md"
+				permaLink = codeName + ".html"
 
-	sectionStart = sectionStart + len(headings[i - 1])
-	sectionText = inFile[sectionStart:sectionEnd]
+		
+		
+		# Extract the sectionText for each command 
+		# ----------------------------------------
+		sectionStart = inFile.index(headings[i - 1])
+		sectionEnd = inFile.index(headings[i])
 
-
-
-	# Parse the sectionText based on MD
-	# ---------------------------------
-
-	sectionText = sectionText.replace("== G-commands ==", '')
-	sectionText = sectionText.replace("== M-commands ==",'')
-
-	sectionText = sectionText.replace("\'\'\'Usage\'\'\'", '## Usage')
-	sectionText = sectionText.replace("\'\'\'Examples\'\'\'", '## Examples')
-	sectionText = sectionText.replace("\'\'\'Example\'\'\'", '## Examples')
-	sectionText = sectionText.replace("\'\'\'Example:\'\'\'", '## Examples')
-	sectionText = sectionText.replace("\'\'\'Examples:\'\'\'", '## Examples')
-	sectionText = sectionText.replace("\'\'\'Parameters\'\'\'", '### Parameters')
-	sectionText = sectionText.replace("\'\'\'Parameters:\'\'\'", '### Parameters')
-	sectionText = sectionText.replace("\'\'\'Extra Parameters\'\'\'", '### Extra Parameters')
-	sectionText = sectionText.replace("\'\'\'Notes\'\'\'", '## Notes')
-	sectionText = sectionText.replace("\'\'\'Order dependency\'\'\'", '## Order Dependency')
-	sectionText = sectionText.replace("\'\'\'Order dependence\'\'\'", '## Order Dependency')
-	sectionText = sectionText.replace("\'\'\'", "`")
-	sectionText = sectionText.replace("^^1^^", "<sub>1</sub>")
-	sectionText = sectionText.replace("^^2^^", "<sub>2</sub>")
-	sectionText = sectionText.replace("[code]", "```\n")
-	sectionText = sectionText.replace("[/code]", "\n```")
-	sectionText = sectionText.replace("===== ", "## ")
-	sectionText = sectionText.replace("=====", "")
+		sectionStart = sectionStart + len(headings[i - 1])
+		sectionText = inFile[sectionStart:sectionEnd]
 
 
-	# Replace section links with permalinks
-	# -------------------------------------
-	if re.search('\[\[Gcode#Section', sectionText, re.IGNORECASE):
-		try: 
-			linkText = re.search('\[\[Gcode#Section(.*?)]]' , sectionText).group(0)	
-			linkCode = re.split('_' , linkText)	
-			pageLink = "[" + linkCode[1] + "](" + linkCode[1] + ".html)"
-			sectionText = sectionText.replace(linkText, pageLink)
-		except:
-			print("ignore" )	
-	# Some logic around tags
-	# ----------------------
-	tags = "["
-	if codeName.startswith("M"):
-		tags = tags + "M-Commands"
-	else:
 
-		tags = tags + "G-Commands"
-
-	if re.search('deprecated', sectionText, re.IGNORECASE):
-		tags = tags + ", Deprecated"
-
-	if re.search('order dependent', sectionText, re.IGNORECASE):
-		tags = tags + ", Order-Dependent"
-
-	if codeName in NETWORKS:
-		tags = tags + ", Network"
-	if codeName in GENERAL:
-		tags = tags + ", General"
-	if codeName in DRIVES:
-		tags = tags + ", Drives"
-	if codeName in AXIS:
-		tags = tags + ", Axis"
-	if codeName in ENDSTOPS:
-		tags = tags + ", Endstops"
-	if codeName in ZPROBE:
-		tags = tags + ", Zprobe"
-	if codeName in HEATERS:
-		tags = tags + ", Heaters"
-	if codeName in FANS:
-		tags = tags + ", Fans"
-	if codeName in TOOLS:
-		tags = tags + ", Tools"
-	if codeName in CUSTOM:  
-		tags = tags + ", Custom"
+		# Parse the sectionText based on MD
+		# ---------------------------------
 	
-	tags = tags + "]"
-	# Write the Jekyll theme front matter
-	# -----------------------------------
-	frontMatter = ["---\n"]
-	frontMatter.append("title: " + codeName + "\n")
-	frontMatter.append("tags: " + tags + " \n" )
-	frontMatter.append("keywords: beta \n" )
-	frontMatter.append("last_updated: " + datetime.date.today().strftime("%B %d, %Y") + " \n" )
-	frontMatter.append("summary: " + headerText + "\n")
-	frontMatter.append("permalink: " + permaLink + "\n")
-	frontMatter.append("toc: false \n")
-
-	frontMatter.append("---\n")
-
-	# Write to File
-	# -------------
-
-	with open(fileName, "w") as myFile:
-		for j in (frontMatter):
-			myFile.write(j)
-
-		myFile.write(sectionText)
+		sectionText = sectionText.replace("== G-commands ==", '')
+		sectionText = sectionText.replace("== M-commands ==",'')
 	
+		sectionText = sectionText.replace("\n# ", '\n* ')
+		sectionText = sectionText.replace("\'\'\'Usage\'\'\'", '## Usage')
+		sectionText = sectionText.replace("\'\'\'Examples\'\'\'", '## Examples')
+		sectionText = sectionText.replace("\'\'\'Example\'\'\'", '## Examples')
+		sectionText = sectionText.replace("\'\'\'Example:\'\'\'", '## Examples')
+		sectionText = sectionText.replace("\'\'\'Examples:\'\'\'", '## Examples')
+		sectionText = sectionText.replace("\'\'\'Parameters\'\'\'", '### Parameters')
+		sectionText = sectionText.replace("\'\'\'Parameters:\'\'\'", '### Parameters')
+		sectionText = sectionText.replace("\'\'\'Extra Parameters\'\'\'", '### Extra Parameters')
+		sectionText = sectionText.replace("\'\'\'Notes\'\'\'", '## Notes')
+		sectionText = sectionText.replace("\'\'\'Order dependency\'\'\'", '## Order Dependency')
+		sectionText = sectionText.replace("\'\'\'Order dependence\'\'\'", '## Order Dependency')
+		sectionText = sectionText.replace("\'\'\'", "`")
+		sectionText = sectionText.replace("^^1^^", "<sub>1</sub>")
+		sectionText = sectionText.replace("^^2^^", "<sub>2</sub>")
+		sectionText = sectionText.replace("[code]", "```\n")
+		sectionText = sectionText.replace("[/code]", "\n```")
+		sectionText = sectionText.replace("===== ", "## ")
+		sectionText = sectionText.replace("=====", "")	
+		
+
+		# Replace section links with permalinks
+		# -------------------------------------
+		if re.search('\[\[Gcode#Section', sectionText, re.IGNORECASE):
+			try: 
+				linkText = re.search('\[\[Gcode#Section(.*?)]]' , sectionText).group(0)	
+				linkCode = re.split('_' , linkText)	
+				pageLink = "[" + linkCode[1] + "](" + linkCode[1] + ".html)"
+				sectionText = sectionText.replace(linkText, pageLink)
+			except:
+				print("ignore" )	
+		# Some logic around tags
+		# ----------------------
+		tags = "["
+		if codeName.startswith("M"):
+			tags = tags + "M-Commands"
+		elif codeName.startswith("G"):	
+			tags = tags + "G-Commands"
+		else:
+			tags = tags + "T-Commands"
+
+		if re.search('deprecated', sectionText, re.IGNORECASE):
+			tags = tags + ", Deprecated"
+
+		if re.search('order dependent', sectionText, re.IGNORECASE):
+			tags = tags + ", Order-Dependent"
+
+		if codeName in NETWORKS:
+			tags = tags + ", Network"
+		if codeName in GENERAL:
+			tags = tags + ", General"
+		if codeName in DRIVES:
+			tags = tags + ", Drives"
+		if codeName in AXIS:
+			tags = tags + ", Axis"
+		if codeName in ENDSTOPS:
+			tags = tags + ", Endstops"
+		if codeName in ZPROBE:
+			tags = tags + ", Zprobe"
+		if codeName in HEATERS:
+			tags = tags + ", Heaters"
+		if codeName in FANS:
+			tags = tags + ", Fans"
+		if codeName in TOOLS:
+			tags = tags + ", Tools"
+		if codeName in CUSTOM:  
+			tags = tags + ", Custom"
+	
+		tags = tags + "]"
+		# Write the Jekyll theme front matter
+		# -----------------------------------
+		frontMatter = ["---\n"]
+		frontMatter.append("title: " + codeName + "\n")
+		frontMatter.append("tags: " + tags + " \n" )
+		frontMatter.append("keywords: beta \n" )
+		frontMatter.append("last_updated: " + datetime.date.today().strftime("%B %d, %Y") + " \n" )
+		frontMatter.append("summary: " + headerText + "\n")
+		frontMatter.append("permalink: " + permaLink + "\n")
+		frontMatter.append("toc: false \n")
+	
+		frontMatter.append("---\n")
+
+		# Write to File
+		# -------------
+
+		with open(fileName, "w") as myFile:
+			for j in (frontMatter):
+				myFile.write(j)
+
+			myFile.write(sectionText)
 	
 
+processSection(headings)
+processSection(tHeadings)
